c563dfe1452c4ddb60638a082001cf8c952dece8
pankajladhar <ladharpankaj@gmail.com>
2018-04-29 14:56:21 +0530

    Added api to read card and wite card in json

---
+++ API/cards.js
@@ -0,0 +1,34 @@
+const app = require('express');
+const path = require('path');
+const util = require('util');
+
+const readFile = util.promisify(require("jsonfile").readFile);
+const writeFile = util.promisify(require("jsonfile").writeFile);
+const cardsRouter = app.Router();
+
+const cards = path.resolve(__dirname, '..', 'data', 'cards.json');
+
+cardsRouter.get('/fetch', (req, res) => {
+    readFile(cards)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(500).send(err))
+});
+
+cardsRouter.post('/create', (req, res) => {
+    let { avatar, style, cardType, alias } = req.body;
+    let request = {
+        id: "a123vdfdr",
+        avatar: avatar || cardType[0].toUpperCase(),
+        style: style,
+        cardType: cardType,
+        alias: alias
+    }
+
+    readFile(cards)
+        .then(data => data)
+        .then((data) => writeFile(cards, [...data, request], { spaces: 2 }))
+        .then(() => res.status(200).send({"message" : "sucessfuly added"}))
+        .catch(err => res.status(500).send(err))
+});
+
+module.exports = cardsRouter;
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cardsAPI = require('./API/cards');
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>{
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.use('/api/v1/cards', cardsAPI)
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
