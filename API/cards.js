2faf5253e2096cddad064768bd1a6c34df9a75b6
pankajladhar <ladharpankaj@gmail.com>
2018-04-29 15:18:01 +0530

    added delete card endpoint

--- API/cards.js
+++ API/cards.js
@@ -17,7 +17,7 @@ cardsRouter.get('/fetch', (req, res) => {
 cardsRouter.post('/create', (req, res) => {
     let { avatar, style, cardType, alias } = req.body;
     let request = {
-        id: "a123vdfdr",
+        id: (new Date()).valueOf(),
         avatar: avatar || cardType[0].toUpperCase(),
         style: style,
         cardType: cardType,
@@ -27,8 +27,17 @@ cardsRouter.post('/create', (req, res) => {
     readFile(cards)
         .then(data => data)
         .then((data) => writeFile(cards, [...data, request], { spaces: 2 }))
-        .then(() => res.status(200).send({"message" : "sucessfuly added"}))
+        .then(() => res.status(200).send({ "message": "sucessfuly added" }))
         .catch(err => res.status(500).send(err))
 });
+
+cardsRouter.delete('/delete', (req, res) => {
+    let cardId = req.body.cardId;
+    readFile(cards)
+        .then((data) => data.filter((d) => d.id !== cardId))
+        .then((data) => writeFile(cards, data, { spaces: 2 }))
+        .then(() => res.status(200).send({ "message": "sucessfuly deleted" }))
+        .catch(err => res.status(500).send(err))
+})
 
 module.exports = cardsRouter;
